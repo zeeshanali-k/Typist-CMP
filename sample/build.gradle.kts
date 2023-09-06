6f4c26a238d410a45f899fbe74983e0d0340dd5c
Zeeshan Ali <zeeshanalik73@gmail.com>
2023-09-06 15:19:38 +0500

    Added Desktop Support

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -46,8 +46,8 @@ kotlin {
                 implementation(compose.material3)
 
                 implementation("org.jetbrains.kotlinx:atomicfu:0.20.2")
-                api(project(":TypistCMP"))
-//                api("tech.dev-scion:typist-cmp:1.1.0")
+//                api(project(":TypistCMP"))
+                api("tech.dev-scion:typist-cmp:1.1.2")
             }
         }
         val androidMain by getting {
